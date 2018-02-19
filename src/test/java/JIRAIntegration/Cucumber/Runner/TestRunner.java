e978158ff588a02eacc2069b526357149582da16
Parasuram1402 <34369655+Parasuram1402@users.noreply.github.com>
2018-02-20 03:34:38 +0530

    upload report file
    share in local directory

--- src/test/java/JIRAIntegration/Cucumber/Runner/TestRunner.java
+++ src/test/java/JIRAIntegration/Cucumber/Runner/TestRunner.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features="features",glue={"JIRAIntegration.Cucumber.StepDefinitions"}, plugin={"pretty","json:E:\\reports\\report.json"})
+@CucumberOptions(features="features",glue={"JIRAIntegration.Cucumber.StepDefinitions"}, plugin={"pretty","report.json"})
 public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
